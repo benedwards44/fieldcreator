5201efd428ce54b392dbab6437f83ff74bbd2ae7
Ben Edwards <ben@edwards.nz>
2023-04-24 10:24:26 +1200

    Remove old requirements

--- fieldcreator/settings.py
+++ fieldcreator/settings.py
@@ -2,18 +2,12 @@ import os
 import dj_database_url
 from pathlib import Path
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 IS_HEROKU = "DYNO" in os.environ
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'ABC123')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'LOCAL')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == '1'
